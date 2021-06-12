25069f3007cd8ec8439b2f5f68b0d74a98ebdc3b
Jay Steinle <jay.steinle62@gmail.com>
2021-06-11 20:42:08 -0500

    Add to home controller

--- src/main/java/com/example/maintenance/controllers/HomeController.java
+++ src/main/java/com/example/maintenance/controllers/HomeController.java
@@ -7,11 +7,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/addjob")
+    public String displayAddJob(Model model){
+        model.addAttribute("title", "AddJob");
+        return "addjob";
+    }
+
+    @GetMapping("/addmachine")
+    public String displayAddMachine(Model model){
+        model.addAttribute("title", "AddMachine");
+        return "addmachine";
+    }
+
+    @GetMapping("/adduser")
+    public String displayAddUser(Model model){
+        model.addAttribute("title", "AddUser");
+        return "adduser";
+    }
 
     @GetMapping
     public String displayIndexForm(Model model){
         model.addAttribute("title", "527 Maintenance");
         return "index";
+    }
+
+    @GetMapping("/jobhistory")
+    public String displayJobHistory(Model model){
+        model.addAttribute("title", "JobHistory");
+        return "jobhistory";
+    }
+
+    @GetMapping("/login")
+    public String displayLogin(Model model){
+        model.addAttribute("title", "Login");
+        return "login";
     }
 
     @GetMapping("/navbar")
